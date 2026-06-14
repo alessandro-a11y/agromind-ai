bec5c648f65a0b848a1ca77f15bcc7072b3b8628
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-06-14 15:28:55 -0300

    feat(infrastructure): add UserConfiguration

---
+++ src/AgroMind.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+using AgroMind.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AgroMind.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Nome).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.Email).IsRequired().HasMaxLength(200);
+        builder.HasIndex(u => u.Email).IsUnique();
+        builder.Property(u => u.SenhaHash).IsRequired();
+        builder.Property(u => u.Role).HasConversion<string>();
+    }
+}
