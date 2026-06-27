83befc32484c89d040880244785aac7091f0fdd1
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-06-27 11:51:50 -0300

    feat(fazendas): adiciona paginação e refatora consultas

--- src/AgroMind.Application/Features/Farms/Queries/GetFarms/GetFarmsQuery.cs
+++ src/AgroMind.Application/Features/Farms/Queries/GetFarms/GetFarmsQuery.cs
@@ -3,14 +3,4 @@ using MediatR;
 
 namespace AgroMind.Application.Features.Farms.Queries.GetFarms;
 
-public record FarmDto(
-    Guid Id,
-    string Nome,
-    string Cidade,
-    string Estado,
-    double? Latitude,
-    double? Longitude,
-    int TotalTalhoes
-);
-
 public record GetFarmsQuery(Guid UserId) : IRequest<Result<List<FarmDto>>>;
