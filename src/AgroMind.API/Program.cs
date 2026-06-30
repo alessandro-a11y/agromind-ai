5e234e7c7b9366789a2f933fa20ff24122ce681f
alessandro-a11y <figueiredoalessandro73@gmail.com>
2026-06-30 11:37:36 -0300

    debug: adiciona log detalhado de falha de autenticacao JWT para diagnostico de CI

--- src/AgroMind.API/Program.cs
+++ src/AgroMind.API/Program.cs
@@ -44,6 +44,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret))
+        };
 
         options.Events = new JwtBearerEvents
         {
@@ -63,7 +64,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 
                 return Task.CompletedTask;
             }
-        };
         };
     });
 
